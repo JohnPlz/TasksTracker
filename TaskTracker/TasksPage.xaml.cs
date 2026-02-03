ada7c602c0a31fc1ead7e823f3016d3213db27de
John Plz <kadafi16@googlemail.com>
2026-02-03 18:44:55 +0100

    Add filtering options for tasks by search text and date range

--- TaskTracker/TasksPage.xaml.cs
+++ TaskTracker/TasksPage.xaml.cs
@@ -8,8 +8,58 @@ namespace TaskTracker;
 public partial class TasksPage : ContentPage
 {
     private readonly TaskRepository _repository;
+    private readonly List<TaskEntry> _allTasks = new();
 
     public ObservableCollection<TaskEntry> Tasks { get; } = new();
+
+    public string SearchText
+    {
+        get => _searchText;
+        set
+        {
+            _searchText = value;
+            OnPropertyChanged();
+            ApplyFilters();
+        }
+    }
+
+    public bool UseDateFilter
+    {
+        get => _useDateFilter;
+        set
+        {
+            _useDateFilter = value;
+            OnPropertyChanged();
+            ApplyFilters();
+        }
+    }
+
+    public DateTime FilterFrom
+    {
+        get => _filterFrom;
+        set
+        {
+            _filterFrom = value;
+            OnPropertyChanged();
+            ApplyFilters();
+        }
+    }
+
+    public DateTime FilterTo
+    {
+        get => _filterTo;
+        set
+        {
+            _filterTo = value;
+            OnPropertyChanged();
+            ApplyFilters();
+        }
+    }
+
+    private string _searchText = string.Empty;
+    private bool _useDateFilter;
+    private DateTime _filterFrom = DateTime.Today.AddDays(-30);
+    private DateTime _filterTo = DateTime.Today;
 
     public TasksPage()
     {
@@ -29,8 +79,35 @@ public partial class TasksPage : ContentPage
 
     private void LoadTasks()
     {
+        _allTasks.Clear();
+        _allTasks.AddRange(_repository.GetAll());
+        ApplyFilters();
+    }
+
+    private void ApplyFilters()
+    {
+        IEnumerable<TaskEntry> query = _allTasks;
+
+        if (!string.IsNullOrWhiteSpace(SearchText))
+        {
+            query = query.Where(item => item.Description.Contains(SearchText.Trim(), StringComparison.OrdinalIgnoreCase));
+        }
+
+        if (UseDateFilter)
+        {
+            var from = FilterFrom.Date;
+            var to = FilterTo.Date;
+            if (from > to)
+            {
+                (from, to) = (to, from);
+            }
+
+            var toInclusive = to.AddDays(1).AddTicks(-1);
+            query = query.Where(item => item.StartTime >= from && item.StartTime <= toInclusive);
+        }
+
         Tasks.Clear();
-        foreach (var item in _repository.GetAll())
+        foreach (var item in query)
         {
             Tasks.Add(item);
         }
@@ -44,6 +121,7 @@ public partial class TasksPage : ContentPage
         }
 
         _repository.Delete(entry.Id);
+        _allTasks.Remove(entry);
         Tasks.Remove(entry);
     }
 }
